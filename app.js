25566fb750c1e63aa2ca1abf0b4339e5f60284b7
Josh Gerbasi <josh.gerbasi@gmail.com>
2013-03-27 22:48:16 -0400

    initial commit with basic node server and mongodb

---
+++ app.js
@@ -0,0 +1,96 @@
+var express = require("express"); // imports express
+var app = express();        // create a new instance of express
+
+// imports the fs module (reading and writing to a text file)
+var fs = require("fs");
+
+// the bodyParser middleware allows us to parse the
+// body of a request
+app.use(express.bodyParser());
+
+// MongoDB connection
+var mongo = require('mongodb');
+var host = 'localhost';
+var port = mongo.Connection.DEFAULT_PORT;
+
+var optionsWithEnableWriteAccess = { w: 1 };
+var dbName = 'testDb';
+
+var client = new mongo.Db(
+    dbName,
+    new mongo.Server(host, port),
+    optionsWithEnableWriteAccess
+);
+
+function openDb(onOpen){
+    client.open(onDbReady);
+
+    function onDbReady(error){
+        if (error)
+            throw error;
+        client.collection('testCollection', onTestCollectionReady);
+    }
+
+    function onTestCollectionReady(error, testCollection){
+        if (error)
+            throw error;
+
+        onOpen(testCollection);
+    }
+}
+
+function closeDb(){
+    client.close();
+}
+
+
+// Asynchronously read file contents, then call callbackFn
+function readFile(filename, defaultData, callbackFn) {
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      console.log("Error reading file: ", filename);
+      data = defaultData;
+    } else {
+      console.log("Success reading file: ", filename);
+    }
+    if (callbackFn) callbackFn(err, data);
+  });
+}
+
+
+// Asynchronously write file contents, then call callbackFn
+function writeFile(filename, data, callbackFn) {
+  fs.writeFile(filename, data, function(err) {
+    if (err) {
+      console.log("Error writing file: ", filename);
+    } else {
+      console.log("Success writing file: ", filename);
+    }
+    if (callbackFn) callbackFn(err);
+  });
+}
+
+
+
+// This is for serving files in the static directory
+app.get("/static/:staticFilename", function (request, response) {
+    response.sendfile("static/" + request.params.staticFilename);
+});
+
+
+function initServer() {
+  //load the categories and tasks
+  // var defaultList = "[]";
+  
+  // readFile("categories.txt", defaultList, function(err, data) {
+  //   categories = JSON.parse(data);
+  // });
+
+  // readFile("tasks.txt", defaultList, function(err, data) {
+  //   tasks = JSON.parse(data);
+  // });
+}
+
+// Initialize server
+initServer();
+app.listen(8889);
